c67ca5f701198110d42f06db532065ebbbb0b1fb
chandrasiribezawada-blip <chandrasiribezawada@gmail.com>
2026-02-27 08:59:22 +0530

    changes done in settings

--- kasukabe/settings.py
+++ kasukabe/settings.py
@@ -126,4 +126,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR/"kasukabe"/"static"]
-STATIC_ROOT = '/home/VarshinBezawada.pythonanywhere.com/final/staticfiles'
+STATIC_ROOT = '/home/VarshinBezawada.pythonanywhere.com/student/staticfiles'
